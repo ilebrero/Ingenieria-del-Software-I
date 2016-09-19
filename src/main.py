1be2a77d42db7ffda89f57373fe88a1c0ddb8610
Francisco Giordano <frangio.1@gmail.com>
2016-09-19 17:05:29 -0300

    datos

--- src/main.py
+++ src/main.py
@@ -4,23 +4,8 @@ from Atributo import *
 from Calificacion import *
 from RegistroDeCalificaciones import *
 from Filtracion import *
+from datos import *
 import sys
-
-wifi = Atributo("Wi-Fi")
-atributos = [wifi]
-
-directorio = DirectorioDeBares()
-
-bar1 = Bar("Tienda del cafe", "Santa Fe y Callao", atributos)
-bar2 = Bar("Cafe Martinez", "Pueyrredon y Cordoba", atributos)
-bar3 = Bar("Starbucks", "Las Heras y Uriburu", [])
-directorio.agregar(bar1)
-directorio.agregar(bar2)
-directorio.agregar(bar3)
-
-registro = RegistroDeCalificaciones()
-registro.agregar(Calificacion(bar1, wifi, 4))
-registro.agregar(Calificacion(bar1, wifi, 5))
 
 def imprimir(bares):
   for bar in bares.listar():
