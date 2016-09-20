1663e968e0307757de1b68adcec9a761b10ffba4
Francisco Giordano <frangio.1@gmail.com>
2016-09-20 18:15:54 -0300

    paraleliza el geocoding

--- src/Ubicacion.py
+++ src/Ubicacion.py
@@ -1,15 +1,20 @@
+import geopy, geopy.distance
+
+_geolocator = geopy.geocoders.GoogleV3("AIzaSyAOPhtRuh0rMVylnRdMn_G529La1eMsybI", timeout=10)
+_distance = geopy.distance.distance
+
+import concurrent.futures
+_executor = concurrent.futures.ThreadPoolExecutor(max_workers=8)
+
 class Ubicacion:
-  import geopy, geopy.distance
-  geolocator = geopy.geocoders.GoogleV3("AIzaSyAOPhtRuh0rMVylnRdMn_G529La1eMsybI", timeout = 5)
-  distance = geopy.distance.distance
 
   def __init__(self, direccion):
     # [TODO] user concurrent.futures
-    self._location = self.geolocator.geocode(direccion + ", Buenos Aires, Argentina")
+    self._location = _executor.submit(_geolocator.geocode, direccion + ", Buenos Aires, Argentina")
     self.direccion = direccion
 
   def __str__(self):
     return self.direccion
 
   def distancia_a(self, otra_ubicacion):
-    return self.distance(self._location.point, otra_ubicacion._location.point)
+    return _distance(self._location.result().point, otra_ubicacion._location.result().point)
