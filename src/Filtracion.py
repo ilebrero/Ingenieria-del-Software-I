ae351367a6a77d4623d1cf881e246c885029c51a
Francisco Giordano <frangio.1@gmail.com>
2016-10-05 17:24:42 -0300

    atributo -> caracteristica

--- src/Filtracion.py
+++ src/Filtracion.py
@@ -2,36 +2,38 @@ from Direccion import *
 from CalculadorDeDistancias import *
 
 class Filtracion:
-  pass
 
-class FiltracionCalificacion(Filtracion):
-
-  def __init__(self, bares, registro, atributo, puntaje):
-    self.atributo = atributo
-    self.puntaje = puntaje
-    self.registro = registro
+  def __init__(self, bares):
     self.bares = bares
 
   def listar(self):
     for bar in self.bares.listar():
       try:
-        if self.puntaje <= self.registro.obtener_promedio(bar, self.atributo):
+        if self.cumple_condicion(bar):
           yield bar
       except:
         pass
 
+class FiltracionCalificacion(Filtracion):
+
+  def __init__(self, bares, registro, atributo, puntaje):
+    super().__init__(bares)
+    self.atributo = atributo
+    self.puntaje = puntaje
+    self.registro = registro
+
+  def cumple_condicion(self, bar):
+      return self.puntaje <= self.registro.obtener_promedio(bar, self.atributo)
+
 class FiltracionDireccion(Filtracion):
 
   def __init__(self, bares, registro, direccion, radio):
-    self.bares = bares
+    super().__init__(bares)
     self.registro = registro
     self.direccion = Direccion(direccion)
     self.calculador_de_distancias = CalculadorDeDistancias()
     self.radio = radio
 
-  def listar(self):
-    for bar in self.bares.listar():
+  def cumple_condicion(self, bar):
       distancia = self.calculador_de_distancias.distancia_entre(bar.direccion, self.direccion)
-      if distancia <= self.radio:
-        yield bar
-
+      return distancia <= self.radio
