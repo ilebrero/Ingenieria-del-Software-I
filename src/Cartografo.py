ecb3f3c97115bc3ca2bf8c9630332045de0c24ff
Francisco Giordano <frangio.1@gmail.com>
2016-10-05 18:31:39 -0300

    integra cartografo

--- src/Cartografo.py
+++ src/Cartografo.py
@@ -1,38 +1,37 @@
-#from Bar import *
+from Bar import *
+from Direccion import *
+from subprocess import Popen
+
+_labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 class Cartografo:
-	
-	def __init__(self):
-		pass
 
-	def marcar_en_mapa(self, unBar):
-		_bares_marcados.append(unBar)
+  def __init__(self):
+      self._bares_marcados = []
 
-	def mostrar_mapa(self, center=None, zoom=10, imgformat="jpeg", maptype="roadmap"):
+  def marcar_en_mapa(self, unBar):
+    self._bares_marcados.append(unBar)
 
-		url_google = "http://maps.google.com/maps/api/staticmap?" # base URL, append query params, separated by &
+  def mostrar_mapa(self):
+    markers = []
 
-		str_size = "size=%ix%i" % (640, 640)  # tuple of ints, up to 640 by 640
-		str_format = "format=" + imgformat
-		str_maptype = "maptype=" + maptype  # roadmap, satellite, hybrid, terrain
+    url_google = "http://maps.google.com/maps/api/staticmap?"
 
-		for i, bar_marcado in enumerate(_bares_marcados):
-			 _markers.append("markers=color:blue|label:" + _labels[i] + "|" + bar_marcado.ubicacion + ", CABA, Argentina")
+    str_size = "size=%ix%i" % (640, 640)
+    str_format = "format=jpeg"
+    str_maptype = "maptype=roadmap"
 
-		str_markers = ""
-		if self._markers != None:						#		for bar in self._bares_marcados:
-			separator = "&"
-			str_markers += separator.join(self._markers)
-		
-		str_sensor = "sensor=false" # must be given, deals with getting loction from mobile device
-	
-		str_options = [str_size, str_format, str_maptype, str_markers, str_sensor]
+    for i, bar_marcado in enumerate(self._bares_marcados):
+       markers.append("markers=color:blue|label:" + _labels[i] + "|" + str(bar_marcado.direccion) + ", Capital Federal, Argentina")
+       print(_labels[i] + " " + bar_marcado.nombre)
 
-		main_separator = "&"
-		url_options = main_separator.join(str_options)
-		url = url_google + url_options
-		return url
+    str_markers = "&".join(markers)
+    
+    str_sensor = "sensor=false"
+  
+    str_options = [str_size, str_format, str_maptype, str_markers, str_sensor]
 
-	_labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	_markers = []
-	_bares_marcados = []
+    url_options = "&".join(str_options)
+    url = url_google + url_options
+    url = url.replace(" ", "+")
+    Popen('curl -s "{}" | display'.format(url), shell=True)
