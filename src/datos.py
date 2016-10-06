d77b25ad91f2a68114998e283dc7bf69a5e3258e
Arribas Joaquin Manuel <jarribas@ws9.labo1>
2016-10-06 17:53:10 -0300

    datos nuevos con calificaciones

--- src/datos.py
+++ src/datos.py
@@ -7,17 +7,20 @@ from Cartografo import *
 
 wifi = Caracteristica("Wi-Fi")
 aircon = Caracteristica("Aire Acondicionado")
+precios = Caracteristica("Precios")
 
 directorio = DirectorioDeBares()
 
 cartografo = Cartografo()
 
 bar1 = Bar("Tienda del cafe", "Santa Fe y Callao", [wifi])
-bar2 = Bar("Cafe Martinez", "Pueyrredon y Cordoba", [wifi, aircon])
-bar3 = Bar("Starbucks", "Las Heras y Uriburu", [aircon])
+bar2 = Bar("Cafe Martinez", "Pueyrredon y Cordoba", [wifi, aircon, precios])
+bar3 = Bar("Starbucks", "Las Heras y Uriburu", [aircon, precios])
 bar4 = Bar("Tips", "Las Heras y Pueyrredon", [wifi])
 bar5 = Bar("Muu lecheria", "Costa Rica y Armenia", [])
 bar6 = Bar("La biela", "Junin y Guido", [wifi])
+bar7 = Bar("Pani", "Junin y Vicente Lopez", [wifi, precios])
+bar8 = Bar("Milli", "Pueyrredon y French", [wifi, aircon,precios])
 
 directorio.agregar(bar1)
 directorio.agregar(bar2)
@@ -25,6 +28,8 @@ directorio.agregar(bar3)
 directorio.agregar(bar4)
 directorio.agregar(bar5)
 directorio.agregar(bar6)
+directorio.agregar(bar7)
+directorio.agregar(bar8)
 
 registro = RegistroDeCalificaciones()
 
@@ -38,3 +43,35 @@ registro.agregar(Calificacion(bar2, wifi, 1))
 registro.agregar(Calificacion(bar4, wifi, 4))
 registro.agregar(Calificacion(bar4, wifi, 4))
 registro.agregar(Calificacion(bar4, wifi, 2))
+
+registro.agregar(Calificacion(bar2, precios, 1))
+registro.agregar(Calificacion(bar2, precios, 2))
+registro.agregar(Calificacion(bar2, precios, 2))
+
+registro.agregar(Calificacion(bar2, aircon, 4))
+registro.agregar(Calificacion(bar2, aircon, 5))
+registro.agregar(Calificacion(bar2, aircon, 4))
+
+registro.agregar(Calificacion(bar8, aircon, 2))
+registro.agregar(Calificacion(bar8, aircon, 4))
+registro.agregar(Calificacion(bar8, aircon, 3))
+
+registro.agregar(Calificacion(bar8, precios, 5))
+registro.agregar(Calificacion(bar8, precios, 4))
+registro.agregar(Calificacion(bar8, precios, 3))
+
+registro.agregar(Calificacion(bar8, wifi, 5))
+registro.agregar(Calificacion(bar8, wifi, 5))
+registro.agregar(Calificacion(bar8, wifi, 3))
+
+registro.agregar(Calificacion(bar7, wifi, 4))
+registro.agregar(Calificacion(bar7, wifi, 4))
+registro.agregar(Calificacion(bar7, wifi, 3))
+
+registro.agregar(Calificacion(bar7, precios, 3))
+registro.agregar(Calificacion(bar7, precios, 4))
+registro.agregar(Calificacion(bar7, precios, 3))
+
+registro.agregar(Calificacion(bar3, precios, 5))
+registro.agregar(Calificacion(bar3, precios, 5))
+registro.agregar(Calificacion(bar3, precios, 3))
